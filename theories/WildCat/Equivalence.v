09b4c2c84c02435d386c54a0e6b839fe650f9a49
Thomas Wilskow Thorbjørnsen <thomas.w.thorbjornsen@gmail.com>
2024-11-06 23:00:51 -0500

    Definition of equivalence by quasi-invertible functor

--- theories/WildCat/Equivalence.v
+++ theories/WildCat/Equivalence.v
@@ -17,3 +17,30 @@ Class IsCatEquiv {A B : Type} (F : A -> B) `{Is1Functor A B F} `{!HasEquivs B} :
 Arguments spl_ess_surj {A B} F {_ _ _ _ _ _ _ _ _ _ _ _} b : rename.
 Arguments hom_spl_ess_surj {A B} F {_ _ _ _ _ _ _ _ _ _ _ _ _ _} f : rename.
 Arguments hom_surjinj {A B} F {_ _ _ _ _ _ _ _ _ _ _ a b f g} p : rename.
+(** I don't think this type is contractlible under Univalence and probably HasMorExt. *)
+
+Class IsCatQuasiInv {A B : Type} (F : A -> B) `{Is1Functor A B F, !HasEquivs A, !HasEquivs B} :=
+  {
+    cat_qinv : B -> A;
+    cat_qinv_is0functor : Is0Functor cat_qinv;
+    cat_qinv_is1functor : Is1Functor cat_qinv;
+    is_cat_sect : NatEquiv (cat_qinv o F) idmap;
+    is_cat_retr : NatEquiv (F o cat_qinv) idmap
+  }.
+
+(* I have no idea if this is more correct than the above type or not. *)
+
+Class IsCatBiInv {A B : Type} (F : A -> B) 
+  `{Is1Functor A B F, !HasEquivs A, !HasEquivs B} :=
+  {
+    cat_qlinv : B -> A;
+    cat_qlinv_is0functor : Is0Functor cat_qlinv;
+    cat_qlinv_is1functor : Is1Functor cat_qlinv;
+    cat_qrinv : B -> A;
+    cat_qrinv_is0functor : Is0Functor cat_qrinv;
+    cat_qrinv_is1functor : Is1Functor cat_qrinv;
+    is_cat_sect_l : NatEquiv (cat_qlinv o F) idmap;
+    is_Cat_retr_r : NatEquiv (F o cat_qrinv) idmap
+  }.
+
+(* Observe that there is a map [IsCatQuasiInv F -> IsCatBiInv F]. These types should hopefully be logically equivalent. *)
